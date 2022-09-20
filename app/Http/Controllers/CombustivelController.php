3ec87ffb923a5c6cc1413a328302dac6556d5fd1
Rafael Schwambach <rafael.schwambach@unidavi.edu.br>
2022-09-20 16:55:12 -0300

    dois

--- app/Http/Controllers/CombustivelController.php
+++ app/Http/Controllers/CombustivelController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\CalculoDeCombustivel;
+use App\Models\CalculoDeCombustivel;
 
 class CombustivelController extends Controller
 {
--- resources/views/resultado.blade.php
+++ resources/views/resultado.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('css/style.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{asset('style.css')}}" type="text/css">
     <title>Document</title>
 </head>
 <body>
@@ -18,7 +18,7 @@
                 </ul>
             </div>
         </div>
-        <a class="botao" href="{{url('/')}}" title="voltar">Voltar</a> 
+        <a class="botao" href="{{url('/')}}" title="voltar">Voltar</a>
     </div>
 </body>
 </html>
